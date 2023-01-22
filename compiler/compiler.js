7d3fed20918a15052c5105ab7af777d025678eac
xternet <pm@xternet.dev>
2023-01-22 19:00:14 +0100

    .

--- compiler/compiler.js
+++ compiler/compiler.js
@@ -5,13 +5,13 @@ const {generateOpcodes}  = require('./generateOpcodes')
 const {generateBytecode} = require('./generateBytecode')
 
 exports.compiler = inputCode => {
-	const inputTkns = tokenizer(inputCode)  //Lexical Analysis: convert chars into tokens/lexems
-	const inputAST  = parser(inputTkns)     //Syntactic Analysis: generate Abstract Syntax Tree (AST)
+  const inputTkns = tokenizer(inputCode)  //Lexical Analysis: convert chars into tokens/lexems
+  const inputAST  = parser(inputTkns)     //Syntactic Analysis: generate Abstract Syntax Tree (AST)
 
-	validate(inputAST)											//Semantic Analysis
+  validate(inputAST) //Semantic Analysis
 
-	const opcodes   = generateOpcodes(inputAST)
-	const bytecode  = generateBytecode(inputAST)
+  const opcodes   = generateOpcodes(inputAST)
+  const bytecode  = generateBytecode(inputAST)
 
-	return [inputTkns, inputAST, opcodes, bytecode]
+  return [inputTkns, inputAST, opcodes, bytecode]
 }
